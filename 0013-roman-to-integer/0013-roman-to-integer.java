beede3cbc76fa8d2f524386d73a85164b4471105
Bahir Hussain <93282626+HussainMBH@users.noreply.github.com>
2023-09-23 11:49:35 +0530

    Time: 3 ms (80.96%), Space: 43.1 MB (98.49%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static final int[] table = new int[256];
+    static {
+        table['I'] = 1;
+        table['V'] = 5;
+        table['X'] = 10;
+        table['L'] = 50;
+        table['C'] = 100;
+        table['D'] = 500;
+        table['M'] = 1000;
+    }
+
+
+    public int romanToInt(String s) {
+        int value = 0;
+        int prev = 0;
+        int current;
+        for (char c : s.toCharArray()) {
+            current = table[c];
+            if (prev < current) {
+                value -= prev + prev;
+            }
+            value += current;
+            prev = current;
+        }
+        return value;
+    }
+}
