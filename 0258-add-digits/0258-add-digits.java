435f66c03ad92aca1b27532dbb15525da0137767
Bahir Hussain <93282626+HussainMBH@users.noreply.github.com>
2023-10-11 12:51:16 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (42.90%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,11 +1,13 @@
 class Solution {
     public int addDigits(int num) {
-        if(num < 9){
+        if(num<9){
             return num;
         }
-        if(num % 9 == 0){
-            return 9;
+        else if(num%9 ==0){
+            return(9);
         }
-        return num %9; // num devide by 9 up to got single digit
+        else{
+            return(num%9);
+        }
     }
 }
