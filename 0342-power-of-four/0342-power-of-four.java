9de8764bdea0c7a6aaed9fe3fe8635958b5c1475
Bahir Hussain <93282626+HussainMBH@users.noreply.github.com>
2023-10-15 19:16:19 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (6.84%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
        int num = 1073741824;
-        if(n<=0){
-            return false; // not consider to - integer
-        }
+       if(n<=0){
+           return false; // not consider to 0 or - value
+       }
         else if(n == 1){
-            return true; // 4^0 = 1
+            return true; // 0 power of 4 = 1
         }
-        else if(num%n ==0 &&(n%10==4 || n%10==6)){
+        else if(num%n == 0 &&(n%10 == 4 || n%10 ==6)){
             return true;
         }
         else{
